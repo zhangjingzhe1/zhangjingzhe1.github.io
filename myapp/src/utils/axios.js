4076c6d7e29c769bd93b42dfb252e66deb27c397
wasd3044 <wasd3044@163.com>
2021-11-10 14:25:27 +0800

    初始化umi

--- myapp/src/utils/axios.js
+++ myapp/src/utils/axios.js
@@ -3,5 +3,10 @@
  */
 
  import axios from 'axios'
+ axios.defaults.withCredentials = true;
+ const service = axios.create({
+     baseURL: 'http://www.zhangjingzhe.net',
+     
+ })
  export default axios;
  
