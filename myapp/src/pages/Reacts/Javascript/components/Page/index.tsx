a83823cfe9897e270a694ca70ba4c6235d6f493c
wasd3044 <wasd3044@163.com>
2021-11-30 11:00:08 +0800

    提取页面组件

---
+++ myapp/src/pages/Reacts/Javascript/components/Page/index.tsx
@@ -0,0 +1,91 @@
+import React, {useState, useEffect} from 'react'
+import { Input, Modal, Row, Form, Button, Tooltip } from 'antd';
+
+import styles from './index.less';
+import icons from '../../../../../iconfont/iconfont.css';
+const {TextArea} = Input;
+const {Item} = Form;
+export default function Page(props) {
+    const {editMode, submit, data, title} = props;
+    const [bEdit, setEdit] = useState(false);
+    const [showOne, setShowOne] = useState(null);
+    const [loading, setLoading] = useState(true)
+    const [form] = Form.useForm();
+    useEffect(() => {
+        setEdit(editMode) 
+        console.log(editMode)
+    }, [editMode])
+    const handleOk = () => {
+      form.validateFields().then(values => {
+        submit && submit(values, showOne).finally(() => handleCancel())
+      })
+    }
+    const handleCancel = () => {
+      setShowOne(null)
+      form.resetFields();
+    }
+    const selectOne = (item) => {
+      setShowOne(JSON.parse(JSON.stringify(item)))
+      const {label, functions} = item;
+      const obj = {}
+      functions.forEach((element, key) => {
+        obj[key] = element;
+      });
+      form.setFieldsValue({label, ...obj})
+    }
+  return (
+    <div>
+        <Row><span className={styles.title}>{title}</span></Row>
+        {
+          editMode &&
+          <Row className={styles.add}><Button type="primary" onClick={() => setShowOne({})}>新增</Button></Row>
+        }
+        <div className={styles.item}>
+          {
+            data?.map((item, key) => {
+              return <Row className={styles.itemLine} key={key}>
+                <h2>{item.label} &nbsp;
+                {
+                  editMode &&
+                  <Tooltip title="修改"> <span className={`${icons.iconfont} ${icons.iconBianji}`} onClick={() => selectOne(item)}></span> </Tooltip>
+                  
+                }
+                </h2>
+                {
+                  item.functions?.map((values, key) => {
+                    return <TextArea autoSize={{minRows: 5}} value={values} key={key}/>
+                  })
+                }
+              </Row>
+            })
+          }
+        </div>
+        <Modal
+          title={!editMode? "查看":showOne?.label ? "修改": "新增"} 
+          visible={!!showOne} 
+          onOk={handleOk} 
+          onCancel={handleCancel}
+          cancelText={'取消'}
+          okText={'确定'}
+        >
+          <Form form={form}>
+              <Item name="label" label="名称">
+                <Input  disabled={!editMode} />
+              </Item>
+              <Row><Button type="primary" onClick={() => {
+                const functions = showOne?.functions || []
+                functions.push('')
+                setShowOne({ ...showOne, functions})
+              }}>新增</Button></Row>
+              {
+                showOne?.functions?.map((item, key) => {
+                  return <Item name={key} label={`方法${key+1}`} key={key}>
+                      <TextArea autoSize={{minRows: 5}} disabled={!editMode} />
+                    </Item>
+                })
+              }
+          </Form>
+        </Modal>
+    </div>
+  );
+}
--- myapp/src/pages/Reacts/Javascript/note/index.tsx
+++ myapp/src/pages/Reacts/Javascript/note/index.tsx
@@ -2,99 +2,35 @@ import React, {useState, useEffect} from 'react'
 import { Input, Modal, Row, Form, Button, Spin } from 'antd';
 import {Link, connect} from 'umi';
 
+import Page from "../components/Page"
+
 import styles from './index.less';
 
 const {TextArea} = Input;
 const {Item} = Form;
 function Note(props) {
     const {editMode, getData, setData, notes, fource} = props;
-    const [bEdit, setEdit] = useState(false);
-    const [showOne, setShowOne] = useState(null);
     const [loading, setLoading] = useState(true)
-    const [form] = Form.useForm();
-    useEffect(() => {
-        setEdit(editMode) 
-        console.log(editMode)
-    }, [editMode])
     useEffect(() => {
         getData({name: "javascript.note"}).then(() => {setLoading(false)})
     }, [])
-    const handleOk = () => {
-      form.validateFields().then(values => {
-        const {label, ...others} = values;
-        const functions = Object.keys(others).map(key => others[key])
-        setData({name: 'javascript.note', fource, data: { ...showOne, label, functions}}).then(() => {
-          handleCancel();
-          setLoading(true)
-          getData({name: "javascript.note"}).then(() => {setLoading(false)})
-        })
+    const handleOk = (values, showOne) => {
+      const {label, ...others} = values;
+      const functions = Object.keys(others).map(key => others[key])
+      return setData({name: 'javascript.note', fource, data: { ...showOne, label, functions}}).then(() => {
+        setLoading(true)
+        return getData({name: "javascript.note"}).then(() => {setLoading(false)})
       })
-    }
-    const handleCancel = () => {
-      setShowOne(null)
-      form.resetFields();
-    }
-    const selectOne = (item) => {
-      setShowOne(JSON.parse(JSON.stringify(item)))
-      const {label, functions} = item;
-      const obj = {}
-      functions.forEach((element, key) => {
-        obj[key] = element;
-      });
-      form.setFieldsValue({label, ...obj})
     }
   return (
     <Spin spinning={loading}>
-        <Row><span className={styles.title}>JavaScript笔记</span></Row>
-        {
-          editMode &&
-          <Row><Button type="primary" onClick={() => setShowOne({})}>新增</Button></Row>
-        }
-        <div className={styles.item}>
-          {
-            notes?.map((item, key) => {
-              return <Row className={styles.itemLine} key={key}>
-                <h2>{item.label} 
-                {
-                  editMode &&
-                  <Button type="primary" onClick={() => selectOne(item)}>修改</Button>
-                }
-                </h2>
-                {
-                  item.functions?.map((values, key) => {
-                    return <TextArea autoSize={{minRows: 5}} value={values} key={key}/>
-                  })
-                }
-              </Row>
-            })
-          }
-        </div>
-        <Modal
-          title={!editMode? "查看":showOne?.label ? "修改": "新增"} 
-          visible={!!showOne} 
-          onOk={handleOk} 
-          onCancel={handleCancel}
-          cancelText={'取消'}
-          okText={'确定'}
-        >
-          <Form form={form}>
-              <Item name="label" label="名称">
-                <Input  disabled={!editMode} />
-              </Item>
-              <Row><Button type="primary" onClick={() => {
-                const functions = showOne?.functions || []
-                functions.push('')
-                setShowOne({ ...showOne, functions})
-              }}>新增</Button></Row>
-              {
-                showOne?.functions?.map((item, key) => {
-                  return <Item name={key} label={`方法${key+1}`} key={key}>
-                      <TextArea autoSize={{minRows: 5}} disabled={!editMode} />
-                    </Item>
-                })
-              }
-          </Form>
-        </Modal>
+        <Page 
+          title="Javascript笔记"
+          data={notes}
+          editMode={editMode}
+          fource={fource}
+          submit={handleOk}
+        />
     </Spin>
   );
 }
